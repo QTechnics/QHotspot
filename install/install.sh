e650ec8cf141623a09618fa085df825e5bd17ff9
Muzaffer AKYIL <makyil@gmail.com>
2020-02-12 19:26:33 +0000

    db5 added

--- install/install.sh
+++ install/install.sh
@@ -174,6 +174,7 @@ if [ ! -f ${PWD}/restarted.qhs ]; then
 	AddPkg p5-Net-SSLeay
 	AddPkg p5-IO-Socket-SSL
     AddPkg p5-Term-ReadKey
+    AddPkg db5
     AddPkg apr
     AddPkg p5-subversion
 	AddPkg python36
